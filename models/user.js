f3413790fcacb120234b96a96462d0ff25112da4
xiokevc <xiokevc@gmail.com>
2025-07-31 09:05:46 -0400

    restaurantschema

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const foodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  restaurant: [restaurantSchema], // Embedded array
+});
+
+module.exports = mongoose.model('User', userSchema);
